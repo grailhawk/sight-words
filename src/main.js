59ebd7b6d7961d85bd71d60100d8981b49e30597
grailhawk <grailhawk@gmail.com>
2020-12-22 23:27:33 -0700

    Add hits/misses and scores

--- src/main.js
+++ src/main.js
@@ -2,42 +2,95 @@ import React, {useState} from 'react'
 import View from './view'
 import sightwords from './words'
 
-const words = []
+// the list of words to test the student on
+const words = setup(sightwords)
 
-function setup () {
-  const w = [...sightwords]
+/**
+ * used to randomize the list of sight words to prevent the studdent from expecting a given order
+ * @param {array} sightwords - list of sight words to test the student with
+ */
+function setup (sightwords) {
+  const w = [...sightwords] // copy the sightwords array so we can distory it as we go
+  const ret = [] // the new randomized array
 
+  // Build the new array
   while (w.length > 0) {
-    console.log(w)
+    // randomly select a word from the old array
     let i = Math.floor(Math.random() * w.length)
-    words.push(w[i])
+    // put the word in the new array
+    ret.push(w[i])
+    // remove the word form the old aray
     w.splice(i, 1)
   }
+
+  // return the new randomize array
+  return ret
 }
-setup()
 
+/**
+ * get the index for the next display word 
+ * @param {number} index - the index of the currently displayed word
+ */
+function next (index) {
+  let nIndex = index + 1
+  // if we are going over the out of bounds of the array go back to 0
+  if (nIndex >= words.length) {
+    nIndex = 0
+  }
+  return nIndex
+}
 
-function App() {
+/**
+ * get the index of the previously displayed word
+ * @param {number} index - the index of the currently displayed word
+ */
+function prev (index) {
+  let nIndex = index - 1
+  if (nIndex <= 0) {
+    nIndex = words.length -1
+  }
+  return nIndex
+}
+
+/**
+ * run the application
+ */
+function Main() {
+  const [hits, setHits] = useState(0)
+  const [misses, setMisses] = useState(0)
   const [index, setIndex] = useState(0)
+  const [status, setStatus] = useState('')
   const word = words[index]
   return <View
     current={index + 1}
     total={words.length}
     word={word}
+    status={status}
+    hits={hits}
+    misses={misses}
     next={_ => {
-      let nIndex = index + 1
-      if (nIndex >= words.length) {
-        nIndex = 0
-      }
-      setIndex(nIndex)
+      setIndex(next(index))
+      setStatus('Next')
     }}
     prev={_ => {
-      let nIndex = index - 1
-      if (nIndex <= 0) {
-        nIndex = words.length -1
-      }
-      setIndex(nIndex)
+      setIndex(prev(index))
+      setStatus("Prev")
+    }}
+    hit={ _ => {
+      setHits(hits + 1)
+      setIndex(next(index))
+      setStatus('Score')
+    }}
+    miss={ _ => {
+      setMisses(misses + 1)
+      setIndex(next(index))
+      setStatus('Miss')
+    }}
+    reset={ _ => {
+      setHits(0)
+      setMisses(0)
+      setIndex(0)
     }} />
 }
 
-export default App;
+export default Main;
--- src/view.js
+++ src/view.js
@@ -3,34 +3,65 @@ import { useSwipeable } from 'react-swipeable';
 import './styles.css';
 
 function View(prop) {
-  // const delta = 30
-  // const [origin, setOrigin] = useState(0)
-  const { current, total, word, next, prev } = prop
+  const {
+    current, // index of the word being displayed
+    total,   // total number of words known
+    word,    // the current sight word being shown
+    status,  // score|miss|next|prev basicly the last action taken
+    next,    // func used to show the next word
+    prev,    // func used to show the previous word
+    hits,    // number of words the player/student has gotten right
+    hit,     // func used to indicate that the player/student has gotten a word correctly
+    misses,  // number of words the player/student has gotten wrong
+    miss,    // func used to indicate that the player/student has gotten a word wrong
+    reset    // fucn used to reset the Hits, Misses, and to go back to the first word.
+  } = prop
+
+  // Build the swipeable handlers
   const handlers = useSwipeable({
-    onSwipedDown: e => { console.log('DOWN ...', e)},
-    onSwipedUp: e => { console.log('UP ...', e)},
+    onSwipedDown: _ => miss(),
+    onSwipedUp: _ => hit(),
     onSwipedLeft: _ => next(),
-    onSwipedRight: _ => prev()
+    onSwipedRight: _ => prev(),
+    preventDefaultTouchmoveEvent: true
   })
-  return (
-    <div
-      { ...handlers }
-      className="app"
-      onClick={next}
-      onContextMenu={ e => {
-        e.preventDefault()
-        prev()
-      }}>
-      <header className="app-header">
-        <div>
-          {current}/{total}
-        </div>
-        <p>
-          {word}
-        </p>
-      </header>
+
+  // Build t he view
+  const view = <div
+    { ...handlers }
+    className="app-header"
+    onClick={next}
+    onContextMenu={ e => {
+      e.preventDefault()
+      prev()
+    }}>
+    <p>
+      {word}
+    </p>
+    <div className='app-report'>
+      <div className='app-report-left'>
+        {hits}
+      </div>
+      <div className='app-report-center'>
+        {current}/{total}
+      </div>
+      <div className='app-report-right'>
+        {misses}
+      </div>
     </div>
-  );
+    <div>
+      {status}
+    </div>
+    <div className='bottom' onClick={ e => {
+        e.preventDefault()
+        e.stopPropagation()
+        reset()
+      }
+    } />
+  </div>
+
+  // render the view
+  return view
 }
 
 export default View;
